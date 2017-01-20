9cdf954281c73cca34a266f61519b19cb99716fc
314pies <ian121363@gmail.com>
2017-01-21 01:42:24 +0800

    Add mode manager . Network instantiation API testing.

---
+++ Assets/Codes/ModeManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModeManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NetworkCreateTest();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void NetworkCreateTest()
+    {
+        PhotonNetwork.Instantiate("TestPrefab", Vector3.zero, Quaternion.identity, 0);
+    }
+}
