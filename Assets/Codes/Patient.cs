7b53bdf52cb0c970295e0b8eb334edcffffc4509
314pies <ian121363@gmail.com>
2017-01-21 14:10:14 +0800

    Wave spawning

---
+++ Assets/Codes/Patient.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patient : MonoBehaviour
+{
+
+    public PhotonView photonView;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateWave", 2.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public float WaveSpeed = 10.0f;
+
+    public void CreateWave()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            GameObject NewWave = PhotonNetwork.Instantiate("Wave", transform.position, Quaternion.identity, 0);
+            Vector3 Speed = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            Speed.Normalize();
+            NewWave.GetComponent<Rigidbody>().velocity = Speed* WaveSpeed;
+        }
+    }
+
+
+}
---
+++ Assets/Codes/RigidBodySync.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PhotonView))]
+public class RigidBodySync : MonoBehaviour
+{
+    [SerializeField]
+    private float SynRate = 5.0f;
+
+    public PhotonView photonView;
+    public Rigidbody rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        photonView = GetComponent<PhotonView>();
+        InvokeRepeating("CheckPos", 1.0f, SynRate);
+        if (PhotonNetwork.isMasterClient)
+        {
+            StartCoroutine(InitialSyn());
+        }
+    }
+
+    void OnValidate()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        photonView = GetComponent<PhotonView>();
+    }
+
+    IEnumerator InitialSyn()
+    {
+        yield return new WaitForSeconds(0.05f);
+        photonView.RPC("InitialSynPos", PhotonTargets.Others, transform.position, rigidBody.velocity);
+    }
+
+
+    [PunRPC]
+    public void InitialSynPos(Vector3 Pos, Vector3 Velocity)
+    {
+        Debug.Log("Initial Sync");
+        transform.position = Pos;
+        rigidBody.velocity = Velocity;
+    }
+
+    public void CheckPos()
+    {
+        if (PhotonNetwork.isMasterClient)
+            photonView.RPC("SynPos", PhotonTargets.Others, transform.position, rigidBody.velocity);
+    }
+
+    [PunRPC]
+    public void SynPos(Vector3 Pos, Vector3 Velocity)
+    {
+        Debug.Log("Sync Invoked: " + Velocity);
+        if (Vector3.Distance(transform.position, Pos) > 0.5f)
+            transform.position = Pos;
+
+        if (Vector3.Distance(Velocity, rigidBody.velocity) > 0.5f)
+            rigidBody.velocity = Velocity;
+    }
+}
