1693ecfe26e59ae5f2abbaed4519076b1c4c37ca
簡志瑋 <dppss92132@gmail.com>
2017-01-21 21:05:51 +0800

    add a plosion effect package.

---
+++ Assets/result.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class result : MonoBehaviour {
+    public Text TeamAwindow;
+    //string aa = oodiofidfiofoidf;
+
+	void Start () {
+        TeamAwindow = GetComponent<Text>();
+        
+        Debug.Log(TeamAwindow);
+	}
+
+	void Update () {
+	
+	}
+}
