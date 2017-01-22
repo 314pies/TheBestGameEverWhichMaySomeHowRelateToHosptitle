34482fa684587f7449f2eb1c4b941028ef5cff1b
314pies <ian121363@gmail.com>
2017-01-22 14:32:23 +0800

    Add tutor

---
+++ Assets/Codes/TutorStr.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class TutorStr : MonoBehaviour
+{
+
+    int CurrentPage = 0;
+    public string[] PageStrs;
+    public Text Context;
+
+    public void NextPage()
+    {
+        if (CurrentPage + 1 < PageStrs.Length)
+            CurrentPage++;
+
+        Context.text = PageStrs[CurrentPage];
+    }
+
+    public void LastPage()
+    {
+        if (CurrentPage -1 >=0)
+            CurrentPage--;
+
+        Context.text = PageStrs[CurrentPage];
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
